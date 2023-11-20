91b6bb5eb325c001121d9d781b6317a6afc5101e
rtef119 <rtef119@gmail.com>
2023-11-20 16:35:06 +0100

    itstep11

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import cv2
+from PIL import Image
 
 image_path = 'cat.jpeg'
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalcatface_extended.xml')
 image = cv2.imread(image_path)
 cat_face = face_cascade.detectMultiScale(image)
-print(cat_face)
 
 
-
-cv2.imshow("Cat", image)
-cv2.waitKey()
+cat = Image.open(image_path)
+glasses = Image.open('glasses.png')
+cat = cat.convert("RGBA")
+glasses = glasses.convert("RGBA")
+for (x, y, w, h) in cat_face:
+    glasses = glasses.resize((w, int(h/3)))
+    cat.paste(glasses, (x, int(y+h/4)), glasses)
+    cat.save("catZokylaramu.png")
+    catZokylaramu = cv2.imread("catZokylaramu.png")
+    cv2.imshow("Cat with glasses", catZokylaramu)
+    cv2.waitKey()
