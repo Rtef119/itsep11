e4abffd17458c49ee19a418cdaad3a0ca9ce39dd
rtef119 <rtef119@gmail.com>
2023-11-20 16:03:13 +0100

    itstep11

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import cv2
 image_path = 'cat.jpeg'
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalcatface_extended.xml')
 image = cv2.imread(image_path)
-cat_face = face_cascade
+cat_face = face_cascade.detectMultiScale(image)
+print(cat_face)
 
 
 
