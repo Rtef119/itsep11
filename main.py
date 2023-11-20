98e95b070e05203c9e4f6f5fb9805cf366f8ff26
rtef119 <rtef119@gmail.com>
2023-11-20 16:02:12 +0100

    itstep11

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import cv2
 
 image_path = 'cat.jpeg'
-face_cascade = cv2.CascadeClassifier
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalcatface_extended.xml')
 image = cv2.imread(image_path)
-
+cat_face = face_cascade
 
 
 
